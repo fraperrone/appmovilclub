fc9efc374d78bc885414f0d22b6a4ba514d8a64f
Fran <fperronerey@gmail.com>
2025-09-09 19:09:55 -0300

    0

---
+++ app/src/main/java/com/example/layouts/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.layouts
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnEnviar: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        supportActionBar?.hide()
+        btnEnviar = findViewById(R.id.btnEnviar)
+        btnEnviar.setOnClickListener {
+            // Lógica para manejar el clic en el botón
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
